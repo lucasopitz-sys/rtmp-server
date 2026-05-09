f2014a42159707150a7e1d8004a174cc1a9245f2
lucasopitz-sys <lucasopitz@gmx.net>
2026-05-10 00:36:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,19 +5,32 @@ app.use(cors());
 
 const clients = [];
 
+app.options('/push', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.end();
+});
+
 app.post('/push', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  req.on('data', chunk => clients.forEach(c => { try { c.write(chunk); } catch(e) {} }));
+  req.on('data', chunk => {
+    clients.forEach(c => { try { c.write(chunk); } catch(e) {} });
+  });
   req.on('end', () => res.end('ok'));
 });
 
 app.get('/stream', (req, res) => {
-  res.setHeader('Content-Type', 'audio/mpeg');
+  res.setHeader('Content-Type', 'audio/webm;codecs=opus');
   res.setHeader('Transfer-Encoding', 'chunked');
   res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   clients.push(res);
   console.log('Client verbunden:', clients.length);
-  req.on('close', () => clients.splice(clients.indexOf(res), 1));
+  req.on('close', () => {
+    const i = clients.indexOf(res);
+    if (i !== -1) clients.splice(i, 1);
+  });
 });
 
 app.listen(process.env.PORT || 8080, () => console.log('Server läuft!'));
