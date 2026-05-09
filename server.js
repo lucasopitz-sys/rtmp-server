a0a306dd330ea22d445ca055491b3386b70689fc
lucasopitz-sys <lucasopitz@gmx.net>
2026-05-10 00:20:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const NodeMediaServer = require('node-media-server');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
 
-const config = {
-  rtmp: { port: 1935, chunk_size: 60000, gop_cache: true, ping: 30, ping_timeout: 60 },
-  http: { port: process.env.PORT || 8080, allow_origin: '*' }
-};
+const clients = [];
 
-const nms = new NodeMediaServer(config);
-nms.run();
-console.log('RTMP Server läuft!');
+app.post('/push', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  req.on('data', chunk => clients.forEach(c => { try { c.write(chunk); } catch(e) {} }));
+  req.on('end', () => res.end('ok'));
+});
+
+app.get('/stream', (req, res) => {
+  res.setHeader('Content-Type', 'audio/mpeg');
+  res.setHeader('Transfer-Encoding', 'chunked');
+  res.setHeader('Cache-Control', 'no-cache');
+  clients.push(res);
+  console.log('Client verbunden:', clients.length);
+  req.on('close', () => clients.splice(clients.indexOf(res), 1));
+});
+
+app.listen(process.env.PORT || 8080, () => console.log('Server läuft!'));
